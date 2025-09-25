a46d470df1f8b8402943a206377ee53ff261fd24
subbu <subbunandyala48@gmail.com>
2025-09-25 19:01:36 +0530

    github action

--- ec2.tf
+++ ec2.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "this" {
     ami = "ami-09c813fb71547fc4f"
     instance_type = "t3.micro" 
-    vpc_security_group_ids = [aws_vpc_security_group_ids] 
+    vpc_security_group_ids = [aws_vpc_security_group_id] 
 
     tags = {
         name = "terraform-demo" 
