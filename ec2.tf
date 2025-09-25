4b5dcb378231c9ae914d0e7da6e7e1b33701492f
subbu <subbunandyala48@gmail.com>
2025-09-25 19:05:14 +0530

    github action

--- ec2.tf
+++ ec2.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "this" {
     ami = "ami-09c813fb71547fc4f"
     instance_type = "t3.micro" 
-    vpc_security_group_ids = [aws_security_group_id]   
+    vpc_security_group_ids = [aws_security_group.this.id]   
 
 
     tags = {
